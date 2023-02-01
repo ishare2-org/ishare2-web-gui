680c78b4a7a0098e78974fc193937b556f1657db
Alex <alex@alex-dev.me>
2023-01-31 20:23:38 -0600

    CTRL+K to focus, ESC to blur searchbox

--- web_app/static/scripts/app.js
+++ web_app/static/scripts/app.js
@@ -1,2 +1,17 @@
 const year = document.getElementById("year");
 year.textContent = new Date().getFullYear();
+
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'k' && (event.ctrlKey || event.metaKey)) {
+        event.preventDefault();
+        document.querySelector('.form-inline.my-2.my-lg-3 input[type="search"]').focus();
+    }
+});
+const searchInput = document.querySelector('.form-inline.my-2.my-lg-3 input[type="search"]');
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' && document.activeElement === searchInput) {
+        event.preventDefault();
+        searchInput.blur();
+    }
+});
