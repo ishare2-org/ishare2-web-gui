fbd31e74f180dd70dda10e67350b727438122af2
Alex <alex@alex-dev.me>
2023-02-04 03:46:01 -0600

    Yet another optimization: Create download endpoints for each file DYNAMICALLY

--- routers/downloads.py
+++ routers/downloads.py
@@ -1,9 +1,8 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 from python_scripts.add_context import add_context
-from python_scripts.ishare2_search_bin import get_bin_list
-from python_scripts.ishare2_search_dynamips import get_dynamips_list
-from python_scripts.ishare2_search_qemu import get_qemu_list
+from python_scripts.get_images import get_images
+from python_scripts.get_images import get_qemu_list
 import os
 
 router = APIRouter()
@@ -11,35 +10,16 @@ router = APIRouter()
 templates = Jinja2Templates(
     directory=os.getenv("TEMPLATES_DIR", "web_app/src"))
 
-
-@router.get("/download/bin", tags=["download"])
-async def get_list_of_bin_images(request: Request):
-    data = {
-        "title": "Bin images - ishare2",
-        "command": get_bin_list()
-    }
-    context = {}
-    context = add_context(context, request, data)
-    return templates.TemplateResponse("pages/lists/bin_list.html", context)
+IMAGE_TYPES = ["bin", "dynamips", "qemu", "docker"]
 
 
-@router.get("/download/dynamips", tags=["download"])
-async def get_list_of_dynamips_images(request: Request):
-    data = {
-        "title": "Dynamips images - ishare2",
-        "command": get_dynamips_list()
-    }
-    context = {}
-    context = add_context(context, request, data)
-    return templates.TemplateResponse("pages/lists/dynamips_list.html", context)
-
-
-@router.get("/download/qemu", tags=["download"])
-async def get_list_of_qemu_images(request: Request):
-    data = {
-        "title": "Qemu images - ishare2",
-        "command": get_qemu_list()
-    }
-    context = {}
-    context = add_context(context, request, data)
-    return templates.TemplateResponse("pages/lists/qemu_list.html", context)
+for image_type in IMAGE_TYPES:
+    @router.get(f"/download/{image_type}", tags=["download"])
+    async def get_image_type(request: Request, image_type=image_type):
+        data = {
+            "title": f"{image_type} images - ishare2",
+            "command": get_images(image_type)
+        }
+        context = {}
+        context = add_context(context, request, data)
+        return templates.TemplateResponse("pages/lists/download.html", context)
