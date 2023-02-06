9f585eb7ae5edef3566c4612e0fa21ca5c3d6940
Alex <alex@alex-dev.me>
2023-02-05 21:44:30 -0600

    Table styles and containers improvements - Added sort functionality when clicking on column header

--- web_app/src/static/scripts/app.js
+++ web_app/src/static/scripts/app.js
@@ -15,3 +15,65 @@ document.addEventListener('keydown', (event) => {
         searchInput.blur();
     }
 });
+
+let headerCells = document.querySelectorAll('th');
+headerCells.forEach(cell => {
+    cell.addEventListener('click', function () {
+        sortTable(cell.cellIndex);
+    });
+});
+
+function sortTable(columnIndex) {
+    let table = document.querySelector('table');
+    let rows = Array.from(table.rows).slice(1);
+
+    // Sort the rows based on the content of the selected column
+    rows.sort(function (a, b) {
+        let valueA = a.cells[columnIndex].textContent;
+        let valueB = b.cells[columnIndex].textContent;
+
+        // Check if the values are numbers
+        valueA = isNaN(parseFloat(valueA)) ? valueA.toLowerCase() : parseFloat(valueA);
+        valueB = isNaN(parseFloat(valueB)) ? valueB.toLowerCase() : parseFloat(valueB);
+
+        if (valueA < valueB) {
+            return -1;
+        }
+        if (valueA > valueB) {
+            return 1;
+        }
+        return 0;
+    });
+
+    // Remove the current rows from the table
+    rows.forEach(row => {
+        table.deleteRow(row.rowIndex);
+    });
+
+    // Re-add the sorted rows to the table
+    rows.forEach(row => {
+        table.appendChild(row);
+    });
+    updateTableStyles();
+}
+
+
+function updateTableStyles() {
+    let table = document.querySelector('table');
+    let rows = Array.from(table.rows).slice(1);
+
+    // Remove the existing styles from the table rows
+    rows.forEach(row => {
+        row.classList.remove('table-active');
+        row.classList.remove('table-secondary');
+    });
+
+    // Add the new styles to the table rows
+    rows.forEach((row, index) => {
+        if (index % 2 === 0) {
+            row.classList.add('table-active');
+        } else {
+            row.classList.add('table-secondary');
+        }
+    });
+}
