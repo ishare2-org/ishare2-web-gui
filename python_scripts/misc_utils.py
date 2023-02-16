577e0def7172198d7dd7ff437b85f45eb1edff70
Alex <alex@alex-dev.me>
2023-02-16 02:47:02 -0600

    Get data by running different commands: - get_version(): gets the version of the installed emulator (name subject to change) - get_version(): gets which version of ishare2 cli is currently installed - iol_license(): gets current .iourc file content (license) - install_ishare2(): runs command to install ishare2 cli. Will be useful for "update ishare2 command later"

--- python_scripts/misc_utils.py
+++ python_scripts/misc_utils.py
@@ -32,6 +32,47 @@ def get_config():
         return json.load(f)
 
 
+def get_version():
+    retcode, stdout, stderr = _run_command(
+        "dpkg -l | grep pnetlab | head -n 1")
+    if retcode == 0 and stdout != "":
+        return "v{}".format(stdout.split()[2])
+    else:
+        return "N/A"
+
+
+def ishare2_cli_version():
+    retcode, stdout, stderr = _run_command(
+        "cat /usr/sbin/ishare2_version")
+    if retcode == 0 and stdout != "":
+        return "{}".format(stdout)
+    else:
+        return "N/A"
+
+
+def iol_license():
+    retcode, stdout, stderr = _run_command(
+        "cat /opt/unetlab/addons/iol/bin/iourc")
+    if retcode == 0 and stdout != "":
+        return stdout
+    else:
+        return "N/A"
+    
+def install_ishare2():
+    command = "curl -o /usr/sbin/ishare2 https://raw.githubusercontent.com/pnetlabrepo/ishare2/main/ishare2 > /dev/null 2>&1 && chmod +x /usr/sbin/ishare2 && ishare2"
+    retcode, stdout, stderr = _run_command(command)
+    if retcode == 0 and "Done" in stdout:
+        return {
+            "status": 0,
+            "message": "iShare2 has been installed successfully",
+        }
+    else:
+        return {
+            "status": 1,
+            "message": "iShare2 installation failed: {}".format(stderr),
+        }
+
+
 def get_changelog_content():
     return get_config()["constants"]["changelog_md_file"]
 
