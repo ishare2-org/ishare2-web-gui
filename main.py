4b9860b223d2a6060709b257698608abf55a08a1
Alex <alex@alex-dev.me>
2023-02-16 02:40:07 -0600

    Moved utils imports to a single line

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from routers import downloads, request_delete, request_download, installed
 from python_scripts.download_csv import download_csv
-from python_scripts.misc_utils import get_help_content
-from python_scripts.misc_utils import get_changelog_gui_content
-from python_scripts.misc_utils import get_changelog_content
-from python_scripts.misc_utils import relicense
+from python_scripts.misc_utils import get_help_content, get_version, get_changelog_gui_content, get_changelog_content, relicense, get_config, install_ishare2
 from python_scripts.add_context import add_context
-from python_scripts.misc_utils import get_config
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
