3aa30b4dab73e5c477adad8e94cce4e76b695e40
Alex <alex@alex-dev.me>
2023-03-05 21:46:27 -0600

    Preparing for production release

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
-from routers import downloads, request_delete, request_download, installed, changelogs
-from python_scripts.download_csv import download_csv
-from python_scripts.misc_utils import get_help_content, get_version, relicense, get_config, install_ishare2
-from python_scripts.command_utils import _run_command
-from python_scripts.add_context import add_context
-from fastapi.staticfiles import StaticFiles
+import os
+import uvicorn
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, Request
-import uvicorn
-import os
+
+from python_scripts.add_context import add_context
+from python_scripts.command_utils import _run_command
+from python_scripts.download_csv import download_csv
+from python_scripts.misc_utils import (
+    get_config,
+    get_help_content,
+    get_version,
+    install_ishare2,
+    relicense,
+)
+
+from routers import (
+    changelogs,
+    downloads,
+    installed,
+    request_delete,
+    request_download,
+)
 
 download_csv()
 
 app = FastAPI(
     title="ishare2 API",
-    version=os.getenv("API_VERSION", "0.0.1")
+    version=os.getenv("API_VERSION", "0.0.1"),
+    debug=True,  # Change to False in production
 )
 
 origins = os.getenv("ALLOWED_ORIGINS", "*").split(",")
@@ -28,50 +43,58 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.mount("/images", StaticFiles(directory=os.getenv("IMAGES_DIR",
-          "./web_app/src/static/images")), name="images")
-app.mount("/styles.css",
-          StaticFiles(directory=os.getenv("STYLES_DIR", "./web_app/src/static/styles")), name="styles")
 app.mount(
-    "/scripts", StaticFiles(directory=os.getenv("SCRIPTS_DIR", "./web_app/src/static/scripts")), name="scripts")
+    "/images", StaticFiles(directory=os.getenv("IMAGES_DIR", "./web_app/src/static/images")), name="images"
+)
+app.mount(
+    "/styles.css",
+    StaticFiles(directory=os.getenv(
+        "STYLES_DIR", "./web_app/src/static/styles")),
+    name="styles",
+)
+app.mount(
+    "/scripts",
+    StaticFiles(directory=os.getenv(
+        "SCRIPTS_DIR", "./web_app/src/static/scripts")),
+    name="scripts",
+)
+
 
 templates = Jinja2Templates(
     directory=os.getenv("TEMPLATES_DIR", "./web_app/src"))
 
-routes = [downloads, request_delete, request_download,
-          installed, changelogs]
+routes = [downloads, request_delete, request_download, installed, changelogs]
 
 for route in routes:
     app.include_router(route.router)
+
 
 @app.get("/", response_class=HTMLResponse, tags=["Root"])
 async def root(request: Request):
     data = {
         "title": "Homepage - ishare2",
     }
-    context = {}
-    context = add_context(context, request, data)
+    context = add_context({}, request, data)
     return templates.TemplateResponse("/pages/index.html", context)
 
 
-@ app.get("/relicense", tags=["Extras"])
+@app.get("/relicense", tags=["Extras"])
 async def get_relicensed(request: Request):
     return relicense()
 
 
-@ app.get("/install/ishare2", tags=["Extras"])
+@app.get("/install/ishare2", tags=["Extras"])
 async def get_install_ishare2(request: Request):
     return install_ishare2()
 
 
-@ app.get("/help", tags=["Help"])
+@app.get("/help", tags=["Help"])
 async def get_help_file(request: Request):
     data = {
         "title": "Help - ishare2",
-        "url": get_help_content()
+        "url": get_help_content(),
     }
-    context = {}
-    context = add_context(context, request, data)
+    context = add_context({}, request, data)
     return templates.TemplateResponse("pages/help.html", context)
 
 
@@ -84,6 +107,6 @@ if __name__ == "__main__":
         "main:app",
         host=HOST,
         port=PORT,
-        reload=True,
+        reload=True,  # Change to False in production
         workers=4,
     )
